e052cff469191d13eb6198e848c765dfb1fc3afa
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2025-02-05 09:25:09 -0700

    Match antialias to canvas

--- src/iframe/renderer/renderer.ts
+++ src/iframe/renderer/renderer.ts
@@ -32,7 +32,7 @@ export class Renderer {
 
   initGL(): void {
     const gl = this.#canvas.getContext('webgl2', {
-      // antialias: false,
+      antialias: false,
       // desynchonized: true, breaks framerate
       // powerPreference: 'high-performance',
     })
