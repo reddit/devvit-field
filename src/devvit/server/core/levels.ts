23bf449cc295b1f92a89404adb2f43fc3eb3a322
Marcus Wood <marcus.wood@reddit.com>
2025-02-24 09:53:41 -0500

    production levels

--- src/devvit/server/core/levels.ts
+++ src/devvit/server/core/levels.ts
@@ -1,72 +1,67 @@
 import type {LevelConfig} from '../../../shared/types/level'
 
 // PRODUCTION
-// export const levels: LevelConfig[] = [
-//   {
-//     id: 0,
-//     subredditId: 't5_dkz0bh',
-//     // @ts-expect-error need to install on the sub and make a post first
-//     postId: '',
-//     subredditName: 'PlayBanField',
-//     theme: {},
-//   },
-//   {
-//     id: 1,
-//     subredditId: 't5_dkz0gv',
-//     // @ts-expect-error need to install on the sub and make a post first
-//     postId: '',
-//     subredditName: 'CantPlayBanField',
-//     theme: {},
-//   },
-//   {
-//     id: 2,
-//     subredditId: 't5_dkz0k9',
-//     // @ts-expect-error need to install on the sub and make a post first
-//     postId: '',
-//     subredditName: 'BananaField',
-//     theme: {},
-//   },
-//   {
-//     id: 3,
-//     subredditId: 't5_dkz0qm',
-//     // @ts-expect-error need to install on the sub and make a post first
-//     postId: '',
-//     subredditName: 'WhyBanField',
-//     theme: {},
-//   },
-//   {
-//     id: 4,
-//     subredditId: 't5_dkz0tb',
-//     // @ts-expect-error need to install on the sub and make a post first
-//     postId: '',
-//     subredditName: 'WhatIsBanField',
-//     theme: {},
-//   },
-// ]
-
 export const levels: LevelConfig[] = [
   {
     id: 0,
-    subredditId: 't5_dii275',
-    postId: 't3_1iwsnfy',
-    subredditName: 'xBanland0',
+    subredditId: 't5_dkz0bh',
+    postId: 't3_1ix3spv',
+    subredditName: 'PlayBanField',
     theme: {},
   },
   {
     id: 1,
-    subredditId: 't5_dii2at',
-    postId: 't3_1iwup5r',
-    subredditName: 'xBanland1',
+    subredditId: 't5_dkz0gv',
+    postId: 't3_1ix3t89',
+    subredditName: 'CantPlayBanField',
     theme: {},
   },
   {
     id: 2,
-    subredditId: 't5_dii2xj',
-    postId: 't3_1iwuphy',
-    subredditName: 'xBanland2',
+    subredditId: 't5_dkz0k9',
+    postId: 't3_1ix3tky',
+    subredditName: 'BananaField',
+    theme: {},
+  },
+  {
+    id: 3,
+    subredditId: 't5_dkz0qm',
+    postId: 't3_1ix3tuy',
+    subredditName: 'WhyBanField',
+    theme: {},
+  },
+  {
+    id: 4,
+    subredditId: 't5_dkz0tb',
+    postId: 't3_1ix3u9l',
+    subredditName: 'WhatIsBanField',
     theme: {},
   },
 ]
+
+// export const levels: LevelConfig[] = [
+//   {
+//     id: 0,
+//     subredditId: 't5_dii275',
+//     postId: 't3_1iwsnfy',
+//     subredditName: 'xBanland0',
+//     theme: {},
+//   },
+//   {
+//     id: 1,
+//     subredditId: 't5_dii2at',
+//     postId: 't3_1iwup5r',
+//     subredditName: 'xBanland1',
+//     theme: {},
+//   },
+//   {
+//     id: 2,
+//     subredditId: 't5_dii2xj',
+//     postId: 't3_1iwuphy',
+//     subredditName: 'xBanland2',
+//     theme: {},
+//   },
+// ]
 
 export const makeLevelRedirect = (levelNumber: number) => {
   const level = levels.find(x => x.id === levelNumber)
