fe7510f7baf9f982589f94b6e1c9e795f5b16da5
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2025-02-26 13:35:41 -0700

    Polyfill crypto in sandbox

--- src/shared/types/v4.ts
+++ src/shared/types/v4.ts
@@ -1,3 +1,5 @@
+import {v4} from 'uuid'
+
 /** UUID v4. */
 export type V4 = ReturnType<Crypto['randomUUID']>
 
@@ -5,5 +7,5 @@ export const noV4: V4 = '00000000-0000-0000-0000-000000000000'
 
 /** Generates a UUID v4. */
 export function V4(): V4 {
-  return crypto.randomUUID()
+  return v4() as V4
 }
