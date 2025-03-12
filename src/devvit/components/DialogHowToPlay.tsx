827855a72cc949bafcef543fc9befdba9b9091f6
Knut Synstad <knut.synstad@reddit.com>
2025-03-12 10:58:35 -0700

    How to play screen in Blocks

---
+++ src/devvit/components/DialogHowToPlay.tsx
@@ -0,0 +1,44 @@
+import {Devvit} from '@devvit/public-api'
+import {localize} from '../../shared/locale'
+import {cssHex, paletteBlack} from '../../shared/theme'
+import {BorderedContainer} from './BorderedContainer'
+import {Dialog} from './Dialog'
+
+type DialogHowToPlayProps = {
+  pixelRatio: number
+}
+
+export function DialogHowToPlay(props: DialogHowToPlayProps): JSX.Element {
+  const width = 256
+  const height = 200
+  const resolution = 2
+
+  return (
+    <Dialog
+      level={0}
+      onPress={() => console.log('do something')}
+      {...props}
+      buttonLabel={localize('how-to-play-dialog-button-label')}
+    >
+      <BorderedContainer
+        height={200}
+        width={256}
+        {...props}
+        backgroundColor={cssHex(paletteBlack)}
+        padding='none'
+      >
+        <vstack height='100%' width='100%' alignment='center middle'>
+          <image
+            imageWidth={`${width * resolution}px`} // Raster size
+            imageHeight={`${height * resolution}px`} // Raster size
+            height={`${height}px`} // Block size
+            width={`${width}px`} // Block size
+            description='How to play illustration: 1. Click a cell to select. 2. Click the claim button. 3. Win?'
+            resizeMode='fill'
+            url='how-to-play-illustration.png'
+          />
+        </vstack>
+      </BorderedContainer>
+    </Dialog>
+  )
+}
