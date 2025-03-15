5de8cdd0a50bf770db6b91d6dcc0f7c7123fe0eb
Knut Synstad <knut.synstad@reddit.com>
2025-03-14 21:03:55 -0700

    Use shared svg factory for Blocks too

--- src/devvit/components/Dialog.tsx
+++ src/devvit/components/Dialog.tsx
@@ -1,6 +1,7 @@
-import {Devvit} from '@devvit/public-api'
+import {Devvit, svg} from '@devvit/public-api'
 import {localize} from '../../shared/locale'
-import {cssHex, paletteBlack, paletteConsole} from '../../shared/theme'
+import {createDialogBadge} from '../../shared/svg-factories/createDialogBadge'
+import {cssHex, paletteConsole} from '../../shared/theme'
 import {
   type Level,
   levelBaseColor,
@@ -69,7 +70,7 @@ export function Dialog(props: DialogProps): JSX.Element {
           </vstack>
 
           <vstack>
-            <spacer height='7px' />
+            <spacer height='6px' />
             <DialogBadge {...props} level={level} />
           </vstack>
         </zstack>
@@ -108,8 +109,7 @@ type DialogBadgeProps = {
 }
 
 function DialogBadge(props: DialogBadgeProps): JSX.Element {
-  const level = props.level ?? 0
-  const SIZE = 24
+  const SIZE = 26
   return (
     <image
       imageHeight={`${SIZE * props.pixelRatio}px`} // Raster size
@@ -118,23 +118,7 @@ function DialogBadge(props: DialogBadgeProps): JSX.Element {
       width={`${SIZE}px`} // Block size
       resizeMode='fill'
       description='r/Field Badge'
-      url={`data:image/svg+xml;charset=UTF-8,
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<!-- Background -->
-<path d="M19.1429 2H4.85714C3.27919 2 2 3.27919 2 4.85714V19.1429C2 20.7208 3.27919 22 4.85714 22H19.1429C20.7208 22 22 20.7208 22 19.1429V4.85714C22 3.27919 20.7208 2 19.1429 2Z" fill="${cssHex(
-        paletteBlack,
-      )}"/>
-
-<!-- Border -->
-<path fill-rule="evenodd" clip-rule="evenodd" d="M4.85714 0H19.1429C21.8254 0 24 2.17462 24 4.85714V19.1429C24 21.8254 21.8254 24 19.1429 24H4.85714C2.17462 24 0 21.8254 0 19.1429V4.85714C0 2.17462 2.17462 0 4.85714 0ZM4.85714 2H19.1429C20.7208 2 22 3.27919 22 4.85714V19.1429C22 20.7208 20.7208 22 19.1429 22H4.85714C3.27919 22 2 20.7208 2 19.1429V4.85714C2 3.27919 3.27919 2 4.85714 2Z" fill="${cssHex(
-        levelBaseColor[level],
-      )}"/>
-
-<!-- Symbol -->
-<path d="M14.0001 6H18V10.0001H14.0001V6Z M14.0001 10.0001L9.99988 10.0013V13.9999H6V18H9.99993V14.0015H13.9998L14.0001 10.0001Z M9.99993 6H6V10.0001L9.99988 10.0013L9.99993 6Z M14.0001 13.9999H18V18H14.0001V13.9999Z" fill="${cssHex(
-        levelHighlightColor[level],
-      )}"/>
-</svg>`}
+      url={svg`${createDialogBadge(props.level ?? 0)}`}
     />
   )
 }
--- src/shared/svg-factories/createDialogBadge.ts
+++ src/shared/svg-factories/createDialogBadge.ts
@@ -1,12 +1,12 @@
 import {cssHex, paletteBlack} from '../theme'
-import {type Level, levelHighlightColor} from '../types/level'
+import {type Level, levelBaseColor, levelHighlightColor} from '../types/level'
 
 export function createDialogBadge(level: Level): string {
-  const border = `<rect width="26" height="26" rx="5" ry="5" fill="${cssHex(levelHighlightColor[level])}" />`
+  const border = `<rect width="26" height="26" rx="5" ry="5" fill="${cssHex(levelBaseColor[level])}" />`
 
   const background = `<rect x="2" y="2" width="22" height="22" rx="3" ry="3" fill="${cssHex(paletteBlack)}" />`
 
-  const symbol = `<path d='M7 7h4v4h4v-4h4v4h-4v4h4v4h-4v-4h-4v4h-4v-4h4v-4h-4Z' fill="${cssHex(levelHighlightColor[level])}" />`
+  const symbol = `<path d="M7 7h4v4h4v-4h4v4h-4v4h4v4h-4v-4h-4v4h-4v-4h4v-4h-4Z" fill="${cssHex(levelHighlightColor[level])}" />`
 
   return `<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">${border}${background}${symbol}</svg>`
 }
