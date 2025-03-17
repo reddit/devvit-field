a015706efa1452e1b9e04f13f1ed0b1013f3e23a
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2025-03-17 08:43:06 -0600

    Fix audio offsets

--- src/iframe/asset-map.ts
+++ src/iframe/asset-map.ts
@@ -23,12 +23,12 @@ export async function AssetMap(): Promise<AssetMap> {
 
   return {
     audio: {
-      '16ItemsInThe15OrLessAtA60sGroceryStore': audioCat.slice(0, 85824),
-      banned: audioCat.slice(85824, 85824 + 16867),
-      claim: audioCat.slice(85824 + 16867, 85824 + 16867 + 12733),
+      '16ItemsInThe15OrLessAtA60sGroceryStore': audioCat.slice(0, 85963),
+      banned: audioCat.slice(85963, 85963 + 16867),
+      claim: audioCat.slice(85963 + 16867, 85963 + 16867 + 12733),
       claimed: audioCat.slice(
-        85824 + 16867 + 12733,
-        85824 + 16867 + 12733 + 12863,
+        85963 + 16867 + 12733,
+        85963 + 16867 + 12733 + 12863,
       ),
     },
     img: {atlas},
