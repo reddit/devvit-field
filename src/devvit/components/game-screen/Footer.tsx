8abfc603f9d75d5b59779a855af5a773c10c3b43
Knut Synstad <knut.synstad@reddit.com>
2025-03-24 15:58:53 -0700

    Reuse shared svg for footer

--- src/devvit/components/game-screen/Footer.tsx
+++ src/devvit/components/game-screen/Footer.tsx
@@ -1,29 +1,24 @@
 import {type Context, Devvit, svg} from '@devvit/public-api'
 import {abbreviateNumber} from '../../../shared/format'
 import {localize} from '../../../shared/locale'
+import {createFooterEnd} from '../../../shared/svg-factories/createFooterEnd'
+import {createFooterMiddle} from '../../../shared/svg-factories/createFooterMiddle'
+import {createFooterStart} from '../../../shared/svg-factories/createFooterStart'
+import {
+  CAP_HEIGHT,
+  CAP_WIDTH,
+  MIDDLE_WIDTH,
+  TITLE_NOTCH_HEIGHT,
+} from '../../../shared/svg-factories/footerSettings'
 import {type Team, teamColor} from '../../../shared/team'
 import {
   cssHex,
   paletteBlack,
-  paletteConsole,
-  paletteShade19,
   paletteShade50,
-  paletteShade80,
-  paletteTint6,
-  paletteTint19,
   paletteWhite,
 } from '../../../shared/theme'
 import {config2} from '../../../shared/types/level'
 import {PixelText} from '../PixelText'
-
-const CAP_HEIGHT = 128
-const CAP_WIDTH = 48
-const MIDDLE_WIDTH = 1048
-const RADIUS = 8
-const STROKE_WEIGHT = 1
-const TITLE_NOTCH_WIDTH = 128
-const TITLE_NOTCH_HEIGHT = 16
-const HALF_STROKE = STROKE_WEIGHT / 2
 
 type FooterProps = {
   pixelRatio: number
@@ -35,16 +30,36 @@ type FooterProps = {
 export function Footer(props: FooterProps, context: Context): JSX.Element {
   const background = (
     <hstack height={`${CAP_HEIGHT}px`} width='100%'>
-      <LeftCap {...props} />
-      <Middle {...props} />
-      <RightCap {...props} />
+      <image
+        imageWidth={CAP_WIDTH * props.pixelRatio}
+        imageHeight={CAP_HEIGHT * props.pixelRatio}
+        width={`${CAP_WIDTH}px`}
+        height={`${CAP_HEIGHT}px`}
+        url={svg`${createFooterStart()}`}
+      />
+      <zstack grow height='100%' alignment='center middle'>
+        <image
+          imageWidth={MIDDLE_WIDTH * props.pixelRatio}
+          imageHeight={CAP_HEIGHT * props.pixelRatio}
+          width={`${MIDDLE_WIDTH}px`}
+          height={`${CAP_HEIGHT}px`}
+          url={svg`${createFooterMiddle()}`}
+        />
+      </zstack>
+      <image
+        imageWidth={CAP_WIDTH * props.pixelRatio}
+        imageHeight={CAP_HEIGHT * props.pixelRatio}
+        width={`${CAP_WIDTH}px`}
+        height={`${CAP_HEIGHT}px`}
+        url={svg`${createFooterEnd()}`}
+      />
     </hstack>
   )
 
   return (
     <zstack
       width='100%'
-      height='128px'
+      height={`${CAP_HEIGHT}px`}
       alignment='center middle'
       onPress={props.onPress}
     >
@@ -103,263 +118,7 @@ export function Footer(props: FooterProps, context: Context): JSX.Element {
           url={`gor-button-${props.claimed ? 'enabled' : 'disabled'}.png`}
           onPress={() => context.ui.navigateTo(config2.leaderboard.url)}
         />
-
-        <spacer height='14px' />
       </vstack>
     </zstack>
-  )
-}
-
-type CapProps = {
-  pixelRatio: number
-}
-
-/*
- * Background: Left Cap
- */
-
-function LeftCap(props: CapProps): JSX.Element {
-  const shadow = [
-    `M${CAP_WIDTH},8`,
-    `H${8 + RADIUS}`,
-    `Q8,8 8,${8 + RADIUS}`,
-    `V${104 - RADIUS}`,
-    `Q8,${104} ${8 + RADIUS},${104}`,
-    `H${CAP_WIDTH}`,
-  ]
-  const bottomTint = [
-    `M${CAP_WIDTH},16`,
-    `H${16 + RADIUS}`,
-    `Q16,16 16,${16 + RADIUS}`,
-    `V${120 - RADIUS}`,
-    `Q16,${120} ${16 + RADIUS},${120}`,
-    `H${CAP_WIDTH}`,
-  ]
-
-  const outerBorder = [
-    `M${CAP_WIDTH},${HALF_STROKE}`,
-    `H${HALF_STROKE}`,
-    `V${CAP_HEIGHT - HALF_STROKE - RADIUS * 2}`,
-    `Q${HALF_STROKE},${CAP_HEIGHT - HALF_STROKE} ${RADIUS * 2},${CAP_HEIGHT - HALF_STROKE}`,
-    `H${CAP_WIDTH}`,
-  ]
-
-  const insetContainer = [
-    `M${CAP_WIDTH},8`,
-    `H${8 + RADIUS}`,
-    `Q8,8 8,${8 + RADIUS}`,
-    `V${96 - RADIUS}`,
-    `Q8,${96} ${8 + RADIUS},${96}`,
-    `H${CAP_WIDTH}`,
-  ]
-
-  return (
-    <image
-      imageWidth={CAP_WIDTH * props.pixelRatio}
-      imageHeight={CAP_HEIGHT * props.pixelRatio}
-      width={`${CAP_WIDTH}px`}
-      height={`${CAP_HEIGHT}px`}
-      url={svg`<svg viewBox="0 0 ${CAP_WIDTH} ${CAP_HEIGHT}" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="${shadow.join('')}" fill="${cssHex(paletteShade19)}" /><path d="${bottomTint.join('')}" fill="${cssHex(paletteBlack)}" /><path d="${outerBorder.join('')}" stroke-width="1" stroke="${cssHex(paletteBlack)}" fill="none" /><path d="${insetContainer.join('')}" stroke-width="1" stroke="${cssHex(paletteBlack)}" fill="${cssHex(paletteConsole)}" /></svg>`}
-    />
-  )
-}
-
-function createTextureLine(x: number, y: number, flip?: boolean): string {
-  return [
-    `M${x},${y}`,
-    `h${flip ? '-' : ''}8`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}8`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}8`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}8`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}22`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}22`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}8,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}3`,
-    `m${flip ? '-' : ''}3,0`,
-    `h${flip ? '-' : ''}22`,
-  ].join('')
-}
-
-/*
- * Background: Middle Section
- */
-
-function Middle(props: CapProps): JSX.Element {
-  const mid = MIDDLE_WIDTH / 2
-
-  const bottomTint = ['M0,96', `H${MIDDLE_WIDTH}`, 'V120', 'H0']
-
-  const outerBorder = [
-    `M0,${HALF_STROKE}`,
-    `H${MIDDLE_WIDTH}`,
-    `M0,${CAP_HEIGHT - HALF_STROKE}`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  const insetContainer = [
-    `M${CAP_WIDTH},8`,
-    `H${MIDDLE_WIDTH}`,
-    `M${CAP_WIDTH},96`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  const wingsDark = [
-    'M0,16',
-    `H${mid - TITLE_NOTCH_WIDTH / 2 - RADIUS}`,
-    'M0,21',
-    `H${mid - TITLE_NOTCH_WIDTH / 2 - RADIUS}`,
-    `M${mid + TITLE_NOTCH_WIDTH / 2 + RADIUS},16`,
-    `H${MIDDLE_WIDTH}`,
-    `M${mid + TITLE_NOTCH_WIDTH / 2 + RADIUS},21`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  const wingsLight = [
-    'M0,17',
-    `H${mid - TITLE_NOTCH_WIDTH / 2 - RADIUS}`,
-    'M0,22',
-    `H${mid - TITLE_NOTCH_WIDTH / 2 - RADIUS}`,
-    `M${mid + TITLE_NOTCH_WIDTH / 2 + RADIUS},17`,
-    `H${MIDDLE_WIDTH}`,
-    `M${mid + TITLE_NOTCH_WIDTH / 2 + RADIUS},22`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  const titleNotch = [
-    `M${mid - TITLE_NOTCH_WIDTH / 2},${HALF_STROKE + 8}`,
-    `L${mid - TITLE_NOTCH_WIDTH / 2 + TITLE_NOTCH_HEIGHT},${HALF_STROKE + 8 + TITLE_NOTCH_HEIGHT}`,
-    `H${mid + TITLE_NOTCH_WIDTH / 2 - TITLE_NOTCH_HEIGHT}`,
-    `L${mid + TITLE_NOTCH_WIDTH / 2},${HALF_STROKE + 8}`,
-  ]
-
-  const cx1 = mid - TITLE_NOTCH_WIDTH / 2
-  const cx2 = mid + TITLE_NOTCH_WIDTH / 2
-
-  const logoNotch = [
-    `M${cx1 - RADIUS},${96}`,
-    `C${cx1},${96} ${cx1},${96 - 12} ${cx1 + RADIUS},${96 - 12}`,
-    `H${cx2 - RADIUS}`,
-    `C${cx2},${96 - 12} ${cx2},${96} ${cx2 + RADIUS},${96}`,
-    `H${cx2 + RADIUS}`,
-    'Z',
-  ]
-
-  const bottomRidgeTop = [
-    `M0,${104}`,
-    `H${cx1 - RADIUS}`,
-    `C${cx1},${104} ${cx1},${104 + 12} ${cx1 + RADIUS},${104 + 12}`,
-    `H${cx2 - RADIUS}`,
-    `C${cx2},${104 + 12} ${cx2},${104} ${cx2 + RADIUS},${104}`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  const bottomRidgeBottom = [
-    `M0,${105}`,
-    `H${cx1 - 1 - RADIUS}`,
-    `C${cx1 - 1},${105} ${cx1 - 1},${105 + 12} ${cx1 - 1 + RADIUS},${105 + 12}`,
-    `H${cx2 + 1 - RADIUS}`,
-    `C${cx2 + 1},${105 + 12} ${cx2 + 1},${105} ${cx2 + 1 + RADIUS},${105}`,
-    `H${MIDDLE_WIDTH}`,
-  ]
-
-  return (
-    <zstack height={`${CAP_HEIGHT}px`} grow alignment='center middle'>
-      <image
-        imageWidth={MIDDLE_WIDTH * props.pixelRatio}
-        imageHeight={CAP_HEIGHT * props.pixelRatio}
-        width={`${MIDDLE_WIDTH}px`}
-        height={`${CAP_HEIGHT}px`}
-        url={svg`<svg viewBox="0 0 ${MIDDLE_WIDTH} ${CAP_HEIGHT}" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="${titleNotch.join('')}${bottomTint.join('')}${logoNotch.join('')}" fill="${cssHex(paletteBlack)}" /><path d="${outerBorder.join('')}${insetContainer.join('')}${createTextureLine(cx2 + 16, 89)}${createTextureLine(cx1 - 16, 89, true)}" stroke-width="1" stroke="${cssHex(paletteBlack)}" fill="none" /><path d="${wingsDark.join('')}" stroke-width="1" stroke="${cssHex(paletteShade80)}" fill="none" /><path d="${wingsLight.join('')}${bottomRidgeTop.join('')}${createTextureLine(cx2 + 16, 88)}${createTextureLine(cx1 - 16, 88, true)}" stroke-width="1" stroke="${cssHex(paletteTint19)}" fill="none" /><path d="${bottomRidgeBottom.join('')}" stroke-width="1" stroke="${cssHex(paletteTint6)}" fill="none" /><path d="${createTextureLine(cx2 + 16, 112)}${createTextureLine(cx1 - 16, 112, true)}" stroke-width="1" stroke="${cssHex(paletteTint19)}" fill="none" /></svg>`}
-      />
-    </zstack>
-  )
-}
-
-/*
- * Background: Right Cap
- */
-
-function RightCap(props: CapProps): JSX.Element {
-  const rightShadowSegments = [
-    'M0,8',
-    `H${CAP_WIDTH - RADIUS - 8}`,
-    `Q${CAP_WIDTH - 8},8 ${CAP_WIDTH - 8},${8 + RADIUS}`,
-    `V${104 - RADIUS}`,
-    `Q${CAP_WIDTH - 8},${104} ${CAP_WIDTH - RADIUS - 8},${104}`,
-    'H0',
-  ]
-  const rightTintSegments = [
-    'M0,16',
-    `H${CAP_WIDTH - RADIUS - 16}`,
-    `Q${CAP_WIDTH - 16},16 ${CAP_WIDTH - 16},${16 + RADIUS}`,
-    `V${120 - RADIUS}`,
-    `Q${CAP_WIDTH - 16},${120} ${CAP_WIDTH - RADIUS - 16},${120}`,
-    'H0',
-  ]
-
-  const rightOuterBorderSegments = [
-    `M0,${HALF_STROKE}`,
-    `H${CAP_WIDTH - HALF_STROKE}`,
-    `V${CAP_HEIGHT - HALF_STROKE - RADIUS * 2}`,
-    `Q${CAP_WIDTH - HALF_STROKE},${CAP_HEIGHT - HALF_STROKE} ${CAP_WIDTH - RADIUS * 2},${CAP_HEIGHT - HALF_STROKE}`,
-    'H0',
-  ]
-
-  const rightInsetContainerSegments = [
-    'M0,8',
-    `H${CAP_WIDTH - RADIUS - 8}`,
-    `Q${CAP_WIDTH - 8},8 ${CAP_WIDTH - 8},${8 + RADIUS}`,
-    `V${96 - RADIUS}`,
-    `Q${CAP_WIDTH - 8},${96} ${CAP_WIDTH - RADIUS - 8},${96}`,
-    'H0',
-  ]
-
-  return (
-    <image
-      imageWidth={CAP_WIDTH * props.pixelRatio}
-      imageHeight={CAP_HEIGHT * props.pixelRatio}
-      width={`${CAP_WIDTH}px`}
-      height={`${CAP_HEIGHT}px`}
-      url={svg`<svg viewBox="0 0 ${CAP_WIDTH} ${CAP_HEIGHT}" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="${rightShadowSegments.join('')}" fill="${cssHex(paletteShade19)}" /><path d="${rightTintSegments.join('')}" fill="${cssHex(paletteBlack)}" /><path d="${rightOuterBorderSegments.join('')}" stroke-width="1" stroke="${cssHex(paletteBlack)}" fill="none" /><path d="${rightInsetContainerSegments.join('')}" stroke-width="1" stroke="${cssHex(paletteBlack)}" fill="${cssHex(paletteConsole)}" /></svg>`}
-    />
   )
 }
