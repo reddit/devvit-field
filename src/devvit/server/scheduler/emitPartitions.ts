9a10fc60e2f194504879a7f54b8d772089f62b09
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2025-03-28 20:16:26 -0600

    Fix compile

--- src/devvit/server/scheduler/emitPartitions.ts
+++ src/devvit/server/scheduler/emitPartitions.ts
@@ -26,8 +26,6 @@ import {
 import {computePreciseScore} from '../core/score.ts'
 import {type Task, WorkQueue} from './workqueue.ts'
 
-const gameOverCheckLockKey = 'gameOverCheckLock'
-
 const buckets = [
   0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0,
   30.0,
