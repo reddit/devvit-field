33377a9c1df6306e19be53a5f056dc32c4c606d3
Stephen Niedzielski <stephen.niedzielski@reddit.com>
2025-04-01 12:06:37 -0600

    Allow users to zoom way out

--- src/iframe/renderer/cam.ts
+++ src/iframe/renderer/cam.ts
@@ -27,7 +27,7 @@ const fieldScaleLvls: readonly number[] = [
   58, 62, 66, 70, 74, 78, 82, 86, 90, 98, 106, 114, 122, 130, 138, 146, 154,
   162, 170, 178, 186, 194, 202, 210, 218, 226, 234, 242, 250,
 ]
-const minUserFieldScaleLvl: number = fieldScaleLvls.indexOf(20)!
+const minUserFieldScaleLvl: number = fieldScaleLvls.indexOf(2)!
 const defaultFieldScaleLvl: number = fieldScaleLvls.indexOf(50)!
 
 export class Cam {
