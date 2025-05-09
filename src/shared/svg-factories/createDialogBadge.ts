f14727fd5b668e04fcb1b339c36eae6dcb6cc831
Knut Synstad <knut.synstad@reddit.com>
2025-05-09 13:38:43 -0700

    Swapped assets

--- src/shared/svg-factories/createDialogBadge.ts
+++ src/shared/svg-factories/createDialogBadge.ts
@@ -6,7 +6,7 @@ export function createDialogBadge(level: Level): string {
 
   const background = `<rect x="2" y="2" width="22" height="22" rx="3" ry="3" fill="${cssHex(paletteBlack)}" />`
 
-  const symbol = `<path d="M7 7h4v4h4v-4h4v4h-4v4h4v4h-4v-4h-4v4h-4v-4h4v-4h-4Z" fill="${cssHex(levelHighlightColor[level])}" />`
+  const symbol = `<path d="M6 6h4v4h-4v-4M11 6h4v4h-4v-4M16 6h4v4h-4v-4M6 11h4v4h-4v-4M11 11h4v4h-4v-4M6 16h4v4h-4v-4Z" fill="${cssHex(levelHighlightColor[level])}" />`
 
   return `<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">${border}${background}${symbol}</svg>`
 }
